5646e9a4da8ba05e13115fe3b39ad7a9f33b3e9d
Izabell <izabell.hallgren@gmail.com>
2024-05-13 11:43:37 +0200

    Added models

---
+++ GreenThumb_Slutprojekt/GreenThumb_Slutprojekt/Models/InstructionModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenThumb_Slutprojekt.Models
+{
+	internal class InstructionModel
+	{
+	}
+}
